bafbe5f14895c4e537161e4f6a299d50e6cf932e
BuildTools <unconfigured@null.spigotmc.org>
2020-05-12 12:35:00 +0200

    added rooms

--- index.js
+++ index.js
@@ -14,15 +14,31 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 });
 
+app.get('/javascript', (req, res) => {
+    res.sendFile(__dirname + '/public/javascript.html')
+});
+
+app.get('/swift', (req, res) => {
+    res.sendFile(__dirname + '/public/swift.html')
+});
+
+app.get('/css', (req, res) => {
+    res.sendFile(__dirname + '/public/css.html')
+});
+
 // Namespace voor chatrooms
 const tech = io.of('/tech');
 
 // Als er een event is
 tech.on("connection", (socket) => {
-    console.log("user connected");
-    socket.on("message", (msg) => {
-        console.log(`message: ${msg}`);
-        tech.emit("message", msg);
+    socket.on('join', (data) => {
+        socket.join(data.room);
+        tech.in(data.room).emit('message', `New user joined ${data.room} room`);
+    })
+
+    socket.on("message", (data) => {
+        console.log(`message: ${data.msg}`);
+        tech.in(data.room).emit("message", data.msg);
     });
 
     socket.on("disconnect", () => {
